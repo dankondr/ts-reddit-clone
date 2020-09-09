ae7ffc20b69b272b24665d6e3e9012d7acb3e135
dankondr <dankondr@icloud.com>
2020-09-09 20:26:13 +0300

    :sparkles: Add apolloserver

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
+import 'reflect-metadata';
+
+import { ApolloServer } from 'apollo-server-express';
 import { MikroORM } from '@mikro-orm/core';
+import { PostResolver } from './resolvers/post';
+import { buildSchema } from 'type-graphql';
+import express from 'express';
 import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  });
 };
 
 main().catch((err) => {
