28f602a97aeb1ab32d7a4449bf7592602fde789a
dankondr <dankondr@icloud.com>
2020-09-09 20:24:00 +0300

    :sparkles: Add graphql PostResolver

---
+++ src/resolvers/post.ts
@@ -0,0 +1,50 @@
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
+import { Post } from 'src/entities/Post';
+import { DBContext } from '../types';
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: DBContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg('id') id: number, @Ctx() { em }: DBContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: DBContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title') title: string,
+    @Ctx() { em }: DBContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    post.title = title;
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg('id') id: number,
+    @Ctx() { em }: DBContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
+  }
+}
